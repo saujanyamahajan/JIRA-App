4cbfa2995315503c519fe634b235990bebd3b634
saujanyamahajan <saujanya.9919@gmail.com>
2021-08-13 20:26:13 +0530

    Complete Project

---
+++ a.js
@@ -0,0 +1,17 @@
+let x = 10;
+let str = "asjnxdkjasnxnaskj" + x + "xnasjknxanskxnask";
+
+// ---------------------------------------
+
+let str2 = `asdjknasjndasdnkasndnaskndjkansk
+asldhnjklasndjknaskjdnkasjn    ${x}
+askjdbnjkasnbdjknasjkndjkasnkdj
+askljdnjkasndjknasjkdnaskndkjnas
+alsijdlasndasnjd`;
+
+console.log(str);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log(str2);
